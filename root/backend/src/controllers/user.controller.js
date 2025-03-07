a81ff5511816f1f997cc94a6c0fc4d698d8d6fc1
Yasha5vi <yashasvi8532@gmail.com>
2025-03-07 15:39:24 +0530

    register user logic

--- root/backend/src/controllers/user.controller.js
+++ root/backend/src/controllers/user.controller.js
@@ -1,10 +1,48 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async (req,res) =>{
-    // console.log("here")
-    res.status(200).json({
-        message:"ok"
+
+    const {firstName, lastName, email, username, password} = req.body
+    const details = [firstName,lastName,email,username,password]
+    
+    if(details.some((field)=>{
+        field?.trim === ""
+    }) || (!details[2].includes('@'))){
+        throw new ApiError(400,"Some details are missing or are not in correct format")
+    }
+
+    // console.log(details)
+    // user pehele se hai 
+    const exits = await User.findOne({
+        $or : [{ username }, { email }]
     })
+    if(exits){
+        throw new ApiError(409,"User already exists");
+    }
+
+    const newUser = await User.create({
+        firstName,
+        lastName,
+        email,
+        username:username.toLowerCase(),
+        password
+    })
+    
+    const createdUser = await User.findById(newUser._id).select(
+        "-password -refreshToken"
+    )
+    if(createdUser){
+        // console.log(createdUser)
+        return res.status(200).json(
+            new ApiResponse(200,createdUser,"user created successfully")
+        )
+    }else{
+        throw new ApiError(500,"DB is not working properly")
+    }
+    
 })
 
 export { registerUser }
--- root/backend/src/models/user.model.js
+++ root/backend/src/models/user.model.js
@@ -48,7 +48,7 @@ const userSchema = new mongoose.Schema(
 // hashing hook
 var saltingRounds = 10
 userSchema.pre("save",async function (next){
-    if((this.isModified("password"))){
+    if(this.isModified("password")){
         this.password = await bcrypt.hash(this.password,saltingRounds)
     }
     next()
